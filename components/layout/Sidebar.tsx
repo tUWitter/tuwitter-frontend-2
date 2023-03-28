5c1975418d7e2937271d1bc56740dde2c275d648
张凡 <zhangfanordomozhang@gmail.com>
2023-03-28 17:12:36 -0400

    cleaned templated files & almost finished sidebar

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,42 @@
+import {BsHouseFill, BsBellFill} from 'react-icons/bs'
+import {FaUser} from 'react-icons/fa'
+import SidebarItem from './SidebarItem';
+import SidebarLogo from './SidebarLogo';
+const Sidebar = () => {
+    const items = [
+        {
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notification',
+            href: '/notification',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/users/123', // TODO: updated for API
+            icon: FaUser
+        }
+    ];
+    return (
+        <div className="col-span-1 h-full pr-4 md:pr-6">
+            <div className='flex flex-col items-end' >
+                <div className='space-y-2 lg:w-[230px]'>
+                    <SidebarLogo/>
+                    {items.map((item) => (
+                        <SidebarItem
+                        key = {item.href}
+                        href = {item.href}
+                        label = {item.label}
+                        icon = {item.icon}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
