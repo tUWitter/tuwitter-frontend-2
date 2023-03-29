cbd94eb18ad5478cfe7680fc0d2e47816614503f
张凡 <zhangfanordomozhang@gmail.com>
2023-03-28 20:58:01 -0400

    added a follow bar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,9 @@
-import {BsHouseFill, BsBellFill} from 'react-icons/bs'
-import {FaUser} from 'react-icons/fa'
+import {BsHouseFill, BsBellFill} from 'react-icons/bs';
+import {FaUser} from 'react-icons/fa';
+import {BiLogOut} from 'react-icons/bi'
 import SidebarItem from './SidebarItem';
-import SidebarLogo from './SidebarLogo';
+import SidebarLogo from './SidebarLogo'; 
+import SidebarTweetButton from './SidebarTweetButton';
 const Sidebar = () => {
     const items = [
         {
@@ -33,6 +35,8 @@ const Sidebar = () => {
                         icon = {item.icon}
                         />
                     ))}
+                    <SidebarItem onClick={() => {}} icon={BiLogOut} label='Logout'/>
+                    <SidebarTweetButton/>
                 </div>
             </div>
         </div>
