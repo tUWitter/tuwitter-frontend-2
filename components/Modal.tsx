19c8fee80692fff1664ad0c2a6cc8a1c8dbac73a
张凡 <zhangfanordomozhang@gmail.com>
2023-04-06 20:56:48 -0400

    fixed undefined bug in modal onsubmit

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, onSubmit, title, body, a
       return;
     }
 
-    onSubmit();
+    onSubmit && onSubmit();
   }, [onSubmit, disabled]);
 
   if (!isOpen) {
