341a9fdb6e190581a4c25e257085a577222a2f21
张凡 <zhangfanordomozhang@gmail.com>
2023-04-07 20:44:15 -0400

    trying on code coverage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css';
-import PostFeed from '@/components/post/PostFeed';
-import Header from '@/components/layout/Header';
-import Form from '@/components/layout/Form';
+import Form from "@/components/layout/Form";
+import Header from "@/components/layout/Header";
+import PostFeed from "@/components/post/PostFeed";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <Header label="Home"/>
-    <Form placeholder="What's happening?"/>
-    <PostFeed />
+      <Header label="Home" />
+      <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
   )
 }
