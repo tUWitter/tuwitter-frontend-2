8c7b5d03b6aa0096020c3d042e5753685a2a2c53
张凡 <zhangfanordomozhang@gmail.com>
2023-04-08 00:53:52 -0400

    added a test

--- cypress/e2e/login_page_spec.cy.ts
+++ cypress/e2e/login_page_spec.cy.ts
@@ -16,4 +16,21 @@ describe('Simple Login', () => {
     after(() => {
       cy.contains('Logout').click()
     })
+  });
+
+  describe('Simple Login', () => {
+    before(() => {
+      cy.visit('/')
+      const email = 'm42zhang@uwaterloo.ca'
+      const password = '123456'
+      cy.contains("Login").click()
+      cy.get('input[placeholder=\"UWaterloo Email\"]').type(email)
+      cy.get('input[placeholder=\"Password\"]').type(password)
+      cy.contains('Sign In').click()
+    })
+
+    it("After logout, the logout button disappear", () => {
+      cy.contains('Logout').click()
+      cy.contains('Logout').should('not.be.visible')
+    })
   })
