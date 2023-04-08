1fdb09f55fd5853a791d0f0991594dd8dd3a8a36
张凡 <zhangfanordomozhang@gmail.com>
2023-04-08 08:57:49 -0400

    remove instrumentation

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  viewportWidth: 1080,
-  viewportHeight: 1080,
   e2e: {
-    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
